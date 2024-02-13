9081064a770b41ccec6219a20a336f12d71f9d4e
Shihab Hasan <shihabhasan.iut@gmail.com>
2024-02-13 00:01:43 -0600

    task-1 - v2

--- task-1/src/01_cleanup.py
+++ task-1/src/01_cleanup.py
@@ -4,6 +4,12 @@ import pandas as pd
 def cleanup():
     df = pd.read_csv('../data_raw/raw_data.csv')
 
+    for index, row in df.iterrows():
+        if df.at[index, 'Frailty'] == 'Y':
+            df.at[index, 'Frailty'] = 1
+        else:
+            df.at[index, 'Frailty'] = 0
+
     # nothing to cleanup
     df.to_csv('../data_clean/clean_data.csv')
 
--- task-1/src/02_analysis.py
+++ task-1/src/02_analysis.py
@@ -4,7 +4,16 @@ import matplotlib.pyplot as plt
 
 def analysis():
     df = pd.read_csv('../data_clean/clean_data.csv')
-    print(df)
+
+    # Plotting a scatter plot with Profit represented by marker colors
+    plt.scatter(df['Age'], df['Grip Strength'], c=df['Frailty'], cmap='viridis', s=100, alpha=0.5)
+    plt.colorbar(label='Frailty')  # Add color bar indicating Profit values
+    plt.title('Sales Over Years (Scatter Plot)')
+    plt.xlabel('Age (Years)')
+    plt.ylabel('Grip Strength (Kg)')
+    plt.grid(True)
+    plt.show()
+    plt.savefig('../result/frailty_based_on_age_and_grip_strength.png', format='png', dpi=300)
 
 
 analysis()
