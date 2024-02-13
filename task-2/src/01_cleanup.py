42003a6d7849a60198375a7b67f2ebf158f9b0ec
Shihab Hasan <shihabhasan.iut@gmail.com>
2024-02-12 23:45:13 -0600

    init commit

---
+++ task-2/src/01_cleanup.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+
+def find_grade(n):
+    if n >= 93:
+        return 'A'
+    elif n >= 90:
+        return 'A-'
+    elif n >= 87:
+        return 'B+'
+    elif n >= 83:
+        return 'B'
+    elif n >= 80:
+        return 'B-'
+    elif n >= 77:
+        return 'C+'
+    elif n >= 73:
+        return 'C'
+    elif n >= 70:
+        return 'C-'
+    elif n >= 67:
+        return 'D+'
+    elif n >= 63:
+        return 'D'
+    elif n >= 60:
+        return 'D-'
+    else:
+        return 'F'
+
+
+def cleanup():
+    df = pd.read_csv('../data_raw/StudentsPerformance.csv')
+    math_grade = []
+    reading_grade = []
+    writing_grade = []
+
+    for index, row in df.iterrows():
+        math_grade.append(find_grade(df.at[index, 'math score']))
+        reading_grade.append(find_grade(df.at[index, 'reading score']))
+        writing_grade.append(find_grade(df.at[index, 'writing score']))
+
+    df['math grade'] = math_grade
+    df['reading grade'] = reading_grade
+    df['writing grade'] = writing_grade
+
+    df.to_csv('../data_clean/clean_data.csv')
+
+
+cleanup()
---
+++ task-2/src/02_analysis.py
@@ -0,0 +1,120 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from tabulate import tabulate
+import seaborn as sns
+
+
+# Graph 1 is console printed
+def graph1(df):
+    grouped_df = df.groupby(['race/ethnicity', 'gender']).size().unstack()
+    table_chart = tabulate(grouped_df, headers='keys', tablefmt='grid')
+    print("Number of Students based on Race/Ethnicity and Gender")
+    print(table_chart)
+
+
+def graph2(df):
+    math_df = df[['gender', 'math grade']]
+
+    reading_df = df[['gender', 'reading grade']]
+
+    writing_df = df[['gender', 'writing grade']]
+
+    # Grouping and unstacking for each subject
+    math_grouped_df = math_df.groupby(['gender', 'math grade']).size().unstack()
+    reading_grouped_df = reading_df.groupby(['gender', 'reading grade']).size().unstack()
+    writing_grouped_df = writing_df.groupby(['gender', 'writing grade']).size().unstack()
+
+    # Plotting the bar charts
+    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8, 12))
+
+    math_grouped_df.plot(kind='bar', ax=axes[0], rot=0)
+    axes[0].set_title('Math Grades By Gender')
+    axes[0].set_xlabel('Gender')
+    axes[0].set_ylabel('Number of Students')
+    axes[0].legend(title='Grades')
+
+    reading_grouped_df.plot(kind='bar', ax=axes[1], rot=0)
+    axes[1].set_title('Reading Grades By Gender')
+    axes[1].set_xlabel('Gender')
+    axes[1].set_ylabel('Number of Students')
+    axes[1].legend(title='Grades')
+
+    writing_grouped_df.plot(kind='bar', ax=axes[2], rot=0)
+    axes[2].set_title('Writing Grades By Gender')
+    axes[2].set_xlabel('Gender')
+    axes[2].set_ylabel('Number of Students')
+    axes[2].legend(title='Grades')
+
+    plt.tight_layout()
+    plt.savefig('grades_based_on_genders.png', format='png', dpi=300)
+
+
+def graph3(df):
+    math_df = df[['parental level of education', 'math grade']]
+
+    reading_df = df[['parental level of education', 'reading grade']]
+
+    writing_df = df[['parental level of education', 'writing grade']]
+
+    # Grouping and unstacking for each subject
+    math_grouped_df = math_df.groupby(['parental level of education', 'math grade']).size().unstack()
+    reading_grouped_df = reading_df.groupby(['parental level of education', 'reading grade']).size().unstack()
+    writing_grouped_df = writing_df.groupby(['parental level of education', 'writing grade']).size().unstack()
+
+    # Plotting the bar charts
+    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8, 12))
+
+    math_grouped_df.plot(kind='bar', ax=axes[0], rot=0)
+    axes[0].set_title('Math Grades By Parental Level of Education')
+    axes[0].set_xlabel('Gender')
+    axes[0].set_ylabel('Number of Students')
+    axes[0].legend(title='Grades')
+
+    reading_grouped_df.plot(kind='bar', ax=axes[1], rot=0)
+    axes[1].set_title('Reading Grades By Parental Level of Education')
+    axes[1].set_xlabel('Gender')
+    axes[1].set_ylabel('Number of Students')
+    axes[1].legend(title='Grades')
+
+    writing_grouped_df.plot(kind='bar', ax=axes[2], rot=0)
+    axes[2].set_title('Writing Grades By Parental Level of Education')
+    axes[2].set_xlabel('Gender')
+    axes[2].set_ylabel('Number of Students')
+    axes[2].legend(title='Grades')
+
+    plt.tight_layout()
+    plt.savefig('grades_based_on_parental_level_of_education.png', format='png', dpi=300)
+
+
+def graph4(df):
+    lunch_counts = df['lunch'].value_counts()
+    plt.figure(figsize=(8, 6))
+    plt.pie(lunch_counts, labels=lunch_counts.index, autopct='%1.1f%%', colors=['green', 'red'])
+    plt.title('Pie Chart of Lunch Types')
+    plt.savefig('pie_chart_of_lunch_types.png', format='png', dpi=300)
+
+
+def graph5(df):
+    plt.figure(figsize=(8, 6))
+    sns.scatterplot(x='reading score', y='writing score', data=df, hue='gender', palette='Set1', alpha=0.7)
+    plt.xlabel('Reading Score')
+    plt.ylabel('Writing Score')
+    plt.title('Scatter Plot of Reading vs. Writing Scores')
+    plt.grid(True)
+    plt.legend(title='Gender')
+    plt.savefig('scatter_plot_of_reading_vs_writing.png', format='png', dpi=300)
+
+
+def analysis():
+    df = pd.read_csv('../data_clean/clean_data.csv')
+    graph1(df)
+    graph2(df)
+    graph3(df)
+    graph4(df)
+    graph5(df)
+
+    print("All Charts are saved in result folder")
+
+
+
+analysis()
